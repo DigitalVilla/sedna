0669ba3f691d27a4c2338fe3c7e275ce1183ec63
DigitalVilla <digitalvilla01@gmail.com>
2021-09-06 17:51:58 -0500

    feat: setup working app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ErrorBoundary } from './components/ErrorBoundary'
+import { Home } from './pages/Home'
+import AppProvider from './store/AppContext'
 
 function App() {
+  const handleReload = () => {
+    window.location.reload()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ErrorBoundary buttonLabel="Reload" onClick={handleReload}>
+      <AppProvider>
+        <Home />
+      </AppProvider>
+    </ErrorBoundary>
+  )
 }
 
-export default App;
+export default App
